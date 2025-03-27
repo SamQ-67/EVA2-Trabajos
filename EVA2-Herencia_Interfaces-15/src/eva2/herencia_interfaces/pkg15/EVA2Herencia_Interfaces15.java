c6f559eac6c14e0f35d355fe8e2d7f190de19f4c
SamQ-67 <ririosazthurex@gmail.com>
2025-03-26 18:53:09 -0600

    Add files via upload

---
+++ EVA2-Herencia_Interfaces-15/src/eva2/herencia_interfaces/pkg15/EVA2Herencia_Interfaces15.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2.herencia_interfaces.pkg15;
+
+/**
+ *
+ * @author ririo
+ */
+public class EVA2Herencia_Interfaces15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+
+interface Prueba{
+}
+interface Prueba2 extends Prueba{
+}
